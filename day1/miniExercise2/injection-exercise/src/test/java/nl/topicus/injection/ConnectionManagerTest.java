8efda4d08c58674e7ffaabc63b284f3128139131
Rob-van-Heuven <teckel008@mail.com>
2026-03-02 16:08:40 +0100

    Add JUnit tests for ConnectionManager and update dependencies

--- day1/miniExercise2/injection-exercise/src/test/java/nl/topicus/injection/ConnectionManagerTest.java
+++ day1/miniExercise2/injection-exercise/src/test/java/nl/topicus/injection/ConnectionManagerTest.java
@@ -1,6 +1,16 @@
 package nl.topicus.injection;
 
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ConnectionManagerTest {
-  
+
+	@Test
+	public void testDataSource()
+	{
+		DataSource ds = ConnectionManager.getDataSource();
+		assertNotNull(ds);
+	}
 }
